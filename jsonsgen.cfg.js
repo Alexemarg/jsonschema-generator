ab1498d6ffdac3917acdd3315718cb2a683f558e
Alexemarg <alexemarg.dev@gmail.com>
2020-11-21 15:46:39 +0700

    Feat: TypeDefinitions generator

--- jsonsgen.cfg.js
+++ jsonsgen.cfg.js
@@ -1,4 +1,13 @@
 module.exports = {
   from: './src',
   to: './dist',
+  tsgen: {
+    enabled: true,
+    to: './dist/types',
+    prefix: '',
+    postfix: '.schema',
+    options: { // Docs: https://www.npmjs.com/package/json-schema-to-typescript#options
+      bannerComment: '',
+    },
+  }
 }
